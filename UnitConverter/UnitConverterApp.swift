8879760e46048748c3c076a542533d60955dd8d9
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-07 09:50:18 -0800

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Garret Poole on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
