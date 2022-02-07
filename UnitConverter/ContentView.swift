6b2799d33fb8cfda294d40ebc2ca6a4a10bc4796
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-07 10:58:56 -0800

    computed property for final conversion; project done

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -11,15 +11,55 @@ struct ContentView: View {
     @State private var unitAmount = 0.0
     @State private var convertingUnit = "m"
     @State private var convertToUnit = "km"
-    
+    @FocusState private var amountFocused: Bool
     let conversionUnits = ["m", "km", "ft", "yd", "mi"]
     
+    var convertedAmount: Double{
+        if convertingUnit == convertToUnit{
+            return unitAmount
+        }
+        //convert initial amount to feet
+        var tempFeetAmount = 0.0
+        switch convertingUnit{
+        case "m":
+            tempFeetAmount = unitAmount*3.28084
+        case "km":
+            tempFeetAmount = unitAmount*3280.84
+        case "ft":
+            tempFeetAmount = unitAmount
+        case "yd":
+            tempFeetAmount = unitAmount*3
+        case "mi":
+            tempFeetAmount = unitAmount*5280
+        default:
+            return -1.0
+        }
+        //convert inital foot amount to necessary unit
+        var finalAmountConverted = 0.0
+        switch convertToUnit{
+        case "m":
+            finalAmountConverted = tempFeetAmount/3.28084
+        case "km":
+            finalAmountConverted = tempFeetAmount/3280.84
+        case "ft":
+            finalAmountConverted = tempFeetAmount
+        case "yd":
+            finalAmountConverted = tempFeetAmount/3
+        case "mi":
+            finalAmountConverted = tempFeetAmount/5280
+        default:
+            return -1.0
+        }
+        return finalAmountConverted
+    }
     
     var body: some View {
         NavigationView{
             Form{
                 Section{
                     TextField("Amount", value: $unitAmount, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($amountFocused)
                     Picker("Starting Unit", selection: $convertingUnit){
                         ForEach(conversionUnits, id: \.self){
                             Text($0)
@@ -40,8 +80,22 @@ struct ContentView: View {
                 } header: {
                     Text("Convert To")
                 }
+                
+                Section{
+                    Text(convertedAmount.formatted())
+                } header: {
+                    Text("Converted to \(convertToUnit)")
+                }
             }
             .navigationTitle("Unit Converter")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    Button("Done"){
+                        amountFocused = false
+                    }
+                }
+            }
         }
     }
 }
