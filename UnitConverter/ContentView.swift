c6f6d1e01abc26ec37a04d989775ce0ae86096f1
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-07 10:27:58 -0800

    unit selection and initial amount

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var unitAmount = 0.0
+    @State private var convertingUnit = "m"
+    @State private var convertToUnit = "km"
+    
+    let conversionUnits = ["m", "km", "ft", "yd", "mi"]
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", value: $unitAmount, format: .number)
+                    Picker("Starting Unit", selection: $convertingUnit){
+                        ForEach(conversionUnits, id: \.self){
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Initial Amount")
+                }
+                
+                Section{
+                    Picker("Convert To", selection: $convertToUnit){
+                        ForEach(conversionUnits, id: \.self){
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Convert To")
+                }
+            }
+            .navigationTitle("Unit Converter")
+        }
     }
 }
 
